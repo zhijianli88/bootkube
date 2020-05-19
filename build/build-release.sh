4021f494a08d9157f03208b3405055ff11a606d5
Mateusz Gozdek <mateusz@kinvolk.io>
2020-05-19 17:13:05 +0200

    build/build-release.sh: use Docker instead of rkt for building
    As rkt is now EoL and should no longer be used.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- build/build-release.sh
+++ build/build-release.sh
@@ -2,8 +2,5 @@
 set -euo pipefail
 
 BOOTKUBE_ROOT=$(git rev-parse --show-toplevel)
-sudo rkt run \
-    --volume bk,kind=host,source=${BOOTKUBE_ROOT} \
-    --mount volume=bk,target=/go/src/github.com/kubernetes-sigs/bootkube \
-    --insecure-options=image docker://golang:1.11.1 --exec /bin/bash -- -c \
-    "cd /go/src/github.com/kubernetes-sigs/bootkube && make release"
+
+docker run -v ${BOOTKUBE_ROOT}:/go/src/github.com/kubernetes-sigs/bootkube golang:1.11.1 /bin/bash -c "cd /go/src/github.com/kubernetes-sigs/bootkube && make release"
