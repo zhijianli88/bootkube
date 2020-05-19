1b1f86dff86fa899f8e2562a760df08232b85ee1
Mateusz Gozdek <mateusz@kinvolk.io>
2020-05-19 17:18:25 +0200

    build/build-release.sh: use /usr/src for building
    Since we now use Go 1.13, we no longer need to build inside GOPATH.
    Actually, we should make sure, that building outside works just fine, as
    this is what usually people do.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- build/build-release.sh
+++ build/build-release.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 BOOTKUBE_ROOT=$(git rev-parse --show-toplevel)
 GOLANG_IMAGE=${GOLANG_IMAGE:-golang:1.13.11}
-SRCDIR="/go/src/github.com/kubernetes-sigs/bootkube"
+SRCDIR="/usr/src/bootkube"
 
 docker run -v ${BOOTKUBE_ROOT}:${SRCDIR} ${GOLANG_IMAGE} /bin/bash -c "cd ${SRCDIR} && make release"
