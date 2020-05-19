cd3555c2252fcb73f34700706ef8c647dd824175
Mateusz Gozdek <mateusz@kinvolk.io>
2020-05-19 17:17:45 +0200

    build/build-release.sh: move src dir to variable
    To make possible change easier and to avoid inconsistencies.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- build/build-release.sh
+++ build/build-release.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 
 BOOTKUBE_ROOT=$(git rev-parse --show-toplevel)
 GOLANG_IMAGE=${GOLANG_IMAGE:-golang:1.13.11}
+SRCDIR="/go/src/github.com/kubernetes-sigs/bootkube"
 
-docker run -v ${BOOTKUBE_ROOT}:/go/src/github.com/kubernetes-sigs/bootkube ${GOLANG_IMAGE} /bin/bash -c "cd /go/src/github.com/kubernetes-sigs/bootkube && make release"
+docker run -v ${BOOTKUBE_ROOT}:${SRCDIR} ${GOLANG_IMAGE} /bin/bash -c "cd ${SRCDIR} && make release"
