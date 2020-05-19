c1263c9d1db623925e3b2ff541c2eb5472e8bf9b
Mateusz Gozdek <mateusz@kinvolk.io>
2020-05-19 17:15:34 +0200

    build/build-release.sh: sync Go version with go.mod file
    We have Go 1.14 available already, but go.mod still mentions 1.13, so we
    should at least update to latest release of it.
    
    Signed-off-by: Mateusz Gozdek <mateusz@kinvolk.io>

--- build/build-release.sh
+++ build/build-release.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 BOOTKUBE_ROOT=$(git rev-parse --show-toplevel)
-GOLANG_IMAGE=${GOLANG_IMAGE:-golang:1.11.1}
+GOLANG_IMAGE=${GOLANG_IMAGE:-golang:1.13.11}
 
 docker run -v ${BOOTKUBE_ROOT}:/go/src/github.com/kubernetes-sigs/bootkube ${GOLANG_IMAGE} /bin/bash -c "cd /go/src/github.com/kubernetes-sigs/bootkube && make release"
